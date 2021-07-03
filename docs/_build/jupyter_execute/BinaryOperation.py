ae11197a5c8087f136b814744223cd5a950a1fbc
davefriedman <davefriedman@protonmail.com>
2021-07-03 17:31:54 -0400

    update

---
+++ docs/_build/jupyter_execute/BinaryOperation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Operation
+
+# __Binary Operation__<br>
+# Let $A$ be a nonempty set.<br>
+# A binary operation on A is a function from $A \times A$ to $A$.
