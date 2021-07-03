02cfb8351523f434b428d43679dd53ef81f2965a
davefriedman <davefriedman@protonmail.com>
2021-07-03 18:32:49 -0400

    update

---
+++ docs/_build/jupyter_execute/Operation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Structures
+
+# ---
+
+# ## Operation
+
+# An operation may also be called a function or a computation.
+
+# __Unary Operation__<br>
+# Let $A$ be a nonempty set.<br>
+# A unary operation on $A$ is a function from $A$ to $A$.
+
+# __Binary Operation__<br>
+# Let $A$ be a nonempty set.<br>
+# A binary operation on $A$ is a function from $A \times A$ to $A$.
+
+# __Ternary Operation__<br>
+# Let $A$ be a nonempty set.<br>
+# A ternary operation on $A$ is a function from $A \times A \times A$ to $A$.
+
+# __N-Ary Operation__<br>
+# Let $A$ be a nonempty set.<br>
+# An $n$-ary operation on $A$ is a function from $\underbrace{A \times A \times ... \times A}_{n \,\text{times}}$ to $A$.
+
+# ---
