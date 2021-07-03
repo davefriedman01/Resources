1558957b8513e32f5898f6052a60da4672c10aef
davefriedman <davefriedman@protonmail.com>
2021-07-03 18:43:39 -0400

    update

--- docs/_build/jupyter_execute/Operation.py
+++ docs/_build/jupyter_execute/Operation.py
@@ -5,24 +5,23 @@
 
 # ---
 
-# ## Operation
+# <span style="color: blue; font-size: 20px;">**Operation**</span>
 
 # An operation may also be called a function or a computation.
 
-# __Unary Operation__<br>
 # Let $A$ be a nonempty set.<br>
-# A unary operation on $A$ is a function from $A$ to $A$.
-
-# __Binary Operation__<br>
-# Let $A$ be a nonempty set.<br>
-# A binary operation on $A$ is a function from $A \times A$ to $A$.
-
-# __Ternary Operation__<br>
-# Let $A$ be a nonempty set.<br>
-# A ternary operation on $A$ is a function from $A \times A \times A$ to $A$.
-
-# __N-Ary Operation__<br>
-# Let $A$ be a nonempty set.<br>
-# An $n$-ary operation on $A$ is a function from $\underbrace{A \times A \times ... \times A}_{n \,\text{times}}$ to $A$.
+# For example, a __unary operation__ on $A$ is a function from $A$ to $A$.<br>
+# A __binary operation__ on $A$ is a function from $A \times A$ to $A$.<br>
+# A __ternary operation__ on $A$ is a function from $A \times A \times A$ to $A$.<br>
+# In general, an __$n$-ary operation__ on $A$ is a function from $\underbrace{A \times A \times ... \times A}_{n \,\text{times}}$ to $A$.<br>
 
 # ---
+
+# <span style="color: blue; font-size: 20px;">**Structure**</span>
+
+# A structure is a nonempty set $A$ with a collection of one or more operations on $A$ and a possibly empty collection of relations on $A$.<br>
+# A structure may also be called an algebraic structure or an algebraic system.<br>
+
+# Examples of structures:
+# 
+# $(\mathbb{R}, +, \cdot, \lt)$
